af374bd1a39fbf55657e83502b51cda84e50665e
Rossalison <96083485+Rossalison@users.noreply.github.com>
2022-05-05 13:24:19 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -31,7 +31,28 @@ function send() {
 }
 
 function check(){
-    get_answer = document.getElementById(input_check_box).value;
-    
-    if ()
+    get_answer = document.getElementById("input_check_box").value;
+
+    if (get_answer == actual_answer)
+    {
+        if (answer_turn == "player1")
+        {
+            update_player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+        else
+        {
+            update_player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+        }
+    }
+
+    if (get_question == actual_question)
+    {
+        if(question_turn == "player1")
+        {
+            question_turn = "player2"
+            
+        }
+    }
 }
